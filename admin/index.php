2945ba2f50feadd1c9ae0f76cdde87f55f82a98f
Etienne Samson <tiennou7@gmail.com>
2011-11-12 01:25:30 +0100

    Polish index page

--- admin/index.php
+++ admin/index.php
@@ -2,9 +2,11 @@
 
 require("../lib/functions.php");
 
-$plugins = Plugin::all(false, LEVEL_PRE);
+$plugins = Plugin::all(false, LEVEL_DEV);
 
-$secrets = Plugin::all(true, LEVEL_PRE);
+$secrets = Plugin::all(true, LEVEL_DEV);
+
+$app = Plugin::get(PLUGIN_IDENTIFIER, QS_ID);
 
 $plugins = array_merge($plugins, $secrets);
 
@@ -12,28 +14,54 @@ $plugins = array_merge($plugins, $secrets);
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-  <title>Plugin administration page</title>
+  <title>Update administration page</title>
 </head>
 <body>
-<h1>Plugin administration page</h1>
+<h1 style="text-decoration:underline">Update administration page</h1>
 
+<p><a href="add.php"><strong>Add update/new plugin</strong></a></p>
+<div style="width:400px;border-top:1px solid #666;margin:20px 0 10px 0;">
+
+<h2 style="text-decoration:underline">Application Updates</h2>
+
+<p><?= count($app->versions) ?> updates</p>
+
+<table>
+  <tr>
+    <th>Version</th>
+    <th>Level</th>
+    <th>Downloads</th>
+    <th>Actions</th>
+  </tr>
+  <?php
+  foreach ($app->versions as $version) {
+    ?><tr>
+      <td>v<?= substr($version->displayVersion(),2) ?></td>
+      <td><?= $version->level ?></td>
+      <td><?= $version->downloads ?></td>
+      <td><a href="update.php?id=<?= $version->identifier ?>&amp;version=<?= $version->version ?>">Edit</a></td>
+    </tr><?php
+  }
+  ?>
+</table>
+<div style="width:400px;border-top:1px solid #666;margin:20px 0 10px 0;">
+  <h2 style="text-decoration:underline">Plugins</h2>
 <p><?= count($plugins) ?> plugins</p>
-
-<a href="add.php">Add plugin</a>
 
 <table>
   <tr>
     <th>Name</th>
     <th>Version</th>
-    <th colspan="2">Actions</th>
+    <th>Downloads</th>
+    <th>Actions</th>
   </tr>
   <?php
   foreach ($plugins as $plugin) {
     ?><tr>
       <td><?= $plugin->name ?></td>
       <td><?= $plugin->displayVersion() ?></td>
-      <td><a href="update.php?id=<?= $plugin->identifier ?>">Edit</a></td>
-      <td><a href="delete.php?id=<?= $plugin->identifier ?>">Delete</a></td>
+      <td><?= $plugin->downloads ?></td>
+      <td><a href="update.php?id=<?= $plugin->identifier ?>&amp;version=<?= $plugin->version ?>">Edit</a></td>
     </tr><?php
   }
   ?>
