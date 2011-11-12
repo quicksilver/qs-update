95298dc7c89c91758481fb076e77b9c15eba6a26
Etienne Samson <tiennou7@gmail.com>
2011-11-12 01:20:20 +0100

    Application updates : allow upload of QS disk image

--- admin/add.php
+++ admin/add.php
@@ -2,26 +2,26 @@
 
 include('../lib/functions.php');
 
-$host = @$_POST['host'];
-if ($host === null)
-  $host = QS_ID;
-
 $mod_date = @$_POST['mod_date'];
 if ($mod_date)
   $mod_date = strftime("%Y-%m-%d %H:%M:%S", strtotime($mod_date));
+
+$plist = null;
+
+$is_app = @$_POST['is_app'];
 
 if (@$_POST['submit'] == "New") {
   $archive_file = @$_FILES["plugin_archive_file"];
   $info_file = @$_FILES["info_plist_file"];
   $image_file = @$_FILES["image_file"];
   $image_ext = @$_POST['image_ext'];
+  $level = @$_POST['app_level'] ? $_POST['app_level'] : 0; /* Plugins get level from plist */
 
   if ($archive_file["error"] !== 0)
-    http_error(400, "Missing plugin_archive_file");
+    http_error(500, "Missing plugin_archive_file");
   if ($info_file["error"] !== 0)
-    http_error(400, "Missing info_plist_file");
+    http_error(500, "Missing info_plist_file");
 
-  $plist = null;
   /* FIXME: Check mime-types / extensions */
   /* Extension preferred, because mime-type is told by client,
   * and usually default to application/octet-stream, which
@@ -29,73 +29,130 @@ if (@$_POST['submit'] == "New") {
   */
   try {
     ob_start();
-    $plist_content = file_get_contents($info_file['tmp_name']);
-
     $plist = new CFPropertyList();
-    $plist->parse($plist_content);
+    $plist_contents = null;
+
+    $plist_contents = file_get_contents($info_file['tmp_name']);
+    $plist->parse($plist_contents);
     ob_end_clean();
   }
   catch (DOMException $e) {
-    error("Exception while parsing: $e");
-    http_error(400, "Exception while parsing plist !");
+    /* Remove the likely offending Escape shortcut present in QS Info.plist and try again. */
+    debug("Failed parsing once, retry...");
+    $plist_contents = str_replace("", "", $plist_contents);
+    try {
+      ob_start();
+      $plist->parse($plist_contents);
+      ob_end_clean();
+    }
+    catch (DOMException $e) {
+      error("Exception while parsing: $e");
+      http_error(500, "Exception while parsing plist !");
+    }
   }
 
   if (!$plist)
-    http_error(400, "Failed to parse plist");
+    http_error(500, "Failed to parse plist");
 
   $dict = $plist->getValue(true);
 
-  debug("Add: $archive_file, $info_file, $image_file, $image_ext, for $host, plist: " . dump_str($dict));
+  if (!$is_app) {
+    debug("Add plugin: $archive_file, $info_file, $image_file, $image_ext, " . dump_str($_POST));
 
-  /* Build our plugin record */
-  $plugin_rec = array();
-  $plugin_rec[PLUGIN_IDENTIFIER] = $dict->get('CFBundleIdentifier')->getValue();
-  $plugin_rec[PLUGIN_HOST] = $host;
-  $plugin_rec[PLUGIN_NAME] = $dict->get('CFBundleName')->getValue();
-  $plugin_rec[PLUGIN_VERSION] = hexstring_to_int($dict->get('CFBundleVersion')->getValue());
-  if ($mod_date)
-    $plugin_rec[PLUGIN_MOD_DATE] = $mod_date;
-  if ($dict->get('CFBundleShortVersionString'))
-    $plugin_rec[PLUGIN_DISPLAY_VERSION] = utf8_decode($dict->get('CFBundleShortVersionString')->getValue());
+    /* Build our plugin record */
+    $record = array();
+    $record[PLUGIN_IDENTIFIER] = $dict->get('CFBundleIdentifier')->getValue();
+    $record[PLUGIN_HOST] = QS_ID;
+    $record[PLUGIN_NAME] = $dict->get('CFBundleName')->getValue();
+    $record[PLUGIN_VERSION] = hexstring_to_int($dict->get('CFBundleVersion')->getValue());
+    if ($mod_date)
+      $record[PLUGIN_MOD_DATE] = $mod_date;
+    if ($dict->get('CFBundleShortVersionString'))
+      $record[PLUGIN_DISPLAY_VERSION] = utf8_decode($dict->get('CFBundleShortVersionString')->getValue());
 
-  $plugin_rec[PLUGIN_LEVEL] = LEVEL_NORMAL;
-  $plugin_rec[PLUGIN_SECRET] = 0;
-  $requirements = $dict->get('QSRequirements');
-  if ($requirements) {
-    $feature_level = $requirements->get('feature');
-    if ($feature_level)
-      $plugin_rec[PLUGIN_LEVEL] = $feature_level->getValue();
+    $record[PLUGIN_LEVEL] = LEVEL_NORMAL;
+    $record[PLUGIN_SECRET] = 0;
+    $requirements = $dict->get('QSRequirements');
+    if ($requirements) {
+      $feature_level = $requirements->get('feature');
+      if ($feature_level) {
+        $record[PLUGIN_LEVEL] = $feature_level->getValue();
+      }
+      $minHostVersion = $requirements->get('minHostVersion');
+      if ($minHostVersion) {
+          $record[PLUGIN_MIN_HOST_VERSION] = $minHostVersion->getValue();
+      }
+      $maxHostVersion = $requirements->get('maxHostVersion');
+      if ($maxHostVersion) {
+          $record[PLUGIN_MAX_HOST_VERSION] = $maxHostVersion->getValue();
+      }
+      $minSystemtVersion = $requirements->get('minSystemVersion');
+      if ($minSystemVersion) {
+          $record[PLUGIN_MIN_SYSTEM_VERSION] = $minSystemVersion->getValue();
+      }
+      $maxSystemVersion = $requirements->get('maxSystemVersion');
+      if ($maxSystemVersion) {
+          $record[PLUGIN_MAX_SYSTEM_VERSION] = $maxSystemVersion->getValue();
+      }
+    }
+    $plugin_info = $dict->get('QSPlugIn');
+    if ($plugin_info) {
+      $secret = $plugin_info->get('secret');
+      if ($secret)
+        $record[PLUGIN_SECRET] = $secret->getValue();
+
+      $author = $plugin_info->get('author');
+      if ($author)
+        $record[PLUGIN_AUTHOR] = $author->getValue();
+
+      $desc = $plugin_info->get('description');
+      if ($desc)
+        $record[PLUGIN_DESCRIPTION] = $desc->getValue();
+    }
+
+    debug("Plugin record generated: " . dump_str($record));
+
+    /* Create the plugin */
+    $plugin = new Plugin($record);
+    $create_options = array(
+      'archive_file' => $archive_file['tmp_name'],
+      'info_file' => $info_file['tmp_name'],
+      'image_file' => $image_file ? $image_file['tmp_name'] : null,
+      'image_ext' => $image_ext,
+    );
+
+    if (!$plugin->create($create_options))
+      http_error(500, "Failed creation of plugin");
+    else
+      http_error(200, "OK");
+  } else {
+    /* Build application update record */
+    debug("Add application: $archive_file, $info_file, $image_file, $image_ext, " . dump_str($_POST));
+    $record = array();
+    $record[PLUGIN_IDENTIFIER] = $dict->get('CFBundleIdentifier')->getValue();
+    $record[PLUGIN_HOST] = "";
+    $record[PLUGIN_NAME] = $dict->get('CFBundleName')->getValue();
+    $record[PLUGIN_VERSION] = hexstring_to_int($dict->get('CFBundleVersion')->getValue());
+    $record[PLUGIN_LEVEL] = $level;
+    if ($mod_date)
+      $record[PLUGIN_MOD_DATE] = $mod_date;
+    if ($dict->get('CFBundleShortVersionString'))
+      $record[PLUGIN_DISPLAY_VERSION] = utf8_decode($dict->get('CFBundleShortVersionString')->getValue());
+
+    debug("Application record generated: " . dump_str($record));
+
+    /* Create the plugin */
+    $plugin = new Plugin($record);
+    $create_options = array(
+      'archive_file' => $archive_file['tmp_name'],
+      'info_file' => $info_file['tmp_name'],
+    );
+
+    if (!$plugin->create($create_options))
+      http_error(500, "Failed creation of application update");
+    else
+      http_error(200, "OK");
   }
-  $plugin_info = $dict->get('QSPlugIn');
-  if ($plugin_info) {
-    $secret = $plugin_info->get('secret');
-    if ($secret)
-      $plugin_rec[PLUGIN_SECRET] = $secret->getValue();
-
-    $author = $plugin_info->get('author');
-    if ($author)
-      $plugin_rec[PLUGIN_AUTHOR] = $author->getValue();
-
-    $desc = $plugin_info->get('description');
-    if ($desc)
-      $plugin_rec[PLUGIN_DESCRIPTION] = $desc->getValue();
-  }
-
-  debug("Plugin record generated: " . dump_str($plugin_rec));
-
-  /* Create the plugin */
-  $plugin = new Plugin($plugin_rec);
-  $create_options = array(
-    'archive_file' => $archive_file['tmp_name'],
-    'info_file' => $info_file['tmp_name'],
-    'image_file' => $image_file ? $image_file['tmp_name'] : null,
-    'image_ext' => $image_ext,
-  );
-
-  if (!$plugin->create($create_options))
-    http_error(500, "Failed creation of plugin");
-  else
-    http_error(200, "OK");
 }
 ?><html>
   <head>
@@ -104,19 +161,22 @@ if (@$_POST['submit'] == "New") {
   </head>
   <body>
     <form name="plugin_add" method="post" enctype="multipart/form-data">
-      <input type="hidden" name="host" value="<?php echo QS_ID;?>" />
       <label for="plugin_archive_file">Plugin ditto archive :</label>
       <input id="plugin_archive_file" name="plugin_archive_file" type="file"/><br />
-
       <label for="info_plist_file">Plugin Info.plist :</label>
       <input id="info_plist_file" name="info_plist_file" type="file"/><br />
-
       <label for="image_file">Plugin Icon :</label>
       <input id="image_file" name="image_file" type="file"/><br />
 
-      <label for="date">Update date (leave empty to use archive modification time) :</label>
-      <input id="mod_date" name="mod_date" type="datetime" size="32"/><br />
+      <input id="is_app" name="is_app" type="checkbox"/><label for="is_app"> Application update</label><br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label for="app_level">Update level :</label>
+      <input type="radio" id="app_level_nor" name="app_level" value="0"><label for="app_level_nor"> Normal</label>&nbsp;&nbsp;
+      <input type="radio" id="app_level_pre" name="app_level" value="1"><label for="app_level_pre"> Pre-release</label>&nbsp;&nbsp;
+      <input type="radio" id="app_level_dev" name="app_level" value="2"><label for="app_level_dev"> Developer</label>
+      </br >
 
+      <label for="date">Update date (leave empty to use archive modification time)</label>
+      <input id="mod_date" name="mod_date" type="datetime" size="32"/><br />
       <input type="submit" name="submit" value="New" />
     </form>
   </body>
