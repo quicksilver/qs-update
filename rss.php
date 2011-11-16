322fd36554b48a6c1b51cece5b77afcefdd34b3d
Etienne Samson <tiennou7@gmail.com>
2011-11-16 17:48:45 +0100

    Fix a few things in the RSS.

--- rss.php
+++ rss.php
@@ -4,20 +4,22 @@ include("lib/functions.php");
 
 $plugins = Plugin::all(false, LEVEL_NORMAL, PLUGIN_MOD_DATE . " DESC");
 
+header('Content-Type: application/rss+xml');
+
 echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>Quicksilver Plugins Repository</title>
     <link>http://qsapp.com/plugins</link>
-    <atom:link href="http://qsapp.com/plugins/rss.xml" rel="self" type="application/rss+xml" />
+    <atom:link href="http://qsapp.com/plugins/rss.php" rel="self" type="application/rss+xml" />
     <description>A list of the most up to date plugins for Quicksilver.</description><?php
     foreach ($plugins as $plugin) {
       $moddate = strftime("%Y-%m-%d", strtotime($plugin->modDate));
       $version = $plugin->displayVersion ? $plugin->displayVersion : int_to_hexstring($plugin->version);
       ?><item>
         <title><?= $plugin->name ?></title>
-        <link>http://qsapp.com/plugins.php</link>
+        <link><?= urlencode($plugin->download_url()); ?></link>
         <description><?= $plugin->name ?> updated to <?= $version ?> on <?= $moddate ?><?php // TODO: Provide changelog ?></description>
         <guid isPermaLink="false"><?= $plugin->plugin_url(); ?></guid>
       </item>
